ba8b4a3525c25c3066f4196a025dddd91f209221
Armin Tirić <atab.craft@gmail.com>
2022-01-24 13:43:01 +0100

    test: unit test for url parse

--- src/url-parse/url-parse.test.ts
+++ src/url-parse/url-parse.test.ts
@@ -4,7 +4,37 @@ import { UrlParse } from '../url-parse/url-parse';
 
 test('i1 not within brackets shold be empty url', async () => {
   const url = await loadFileAndProcessTextForUrl('./input/i1.txt');
+  expect(url).toBe("")
+});
+
+test('i2 within brackets shold detect url', async () => {
+  const url = await loadFileAndProcessTextForUrl('./input/i2.txt');
   expect(url).toBe("www.google.com")
+});
+
+test('i3 within brackets shold detect url', async () => {
+  const url = await loadFileAndProcessTextForUrl('./input/i3.txt');
+  expect(url).toBe("www.google.com")
+});
+
+test('i4 multiple urls within brackets shold detect last one', async () => {
+  const url = await loadFileAndProcessTextForUrl('./input/i4.txt');
+  expect(url).toBe("www.second.com")
+});
+
+test('i5 multiple levels of bracket and urls within brackets shold detect last one', async () => {
+  const url = await loadFileAndProcessTextForUrl('./input/i5.txt');
+  expect(url).toBe("www.second.com")
+});
+
+test('i6 multiple levels of bracket and urls within brackets shold detect last one', async () => {
+  const url = await loadFileAndProcessTextForUrl('./input/i6.txt');
+  expect(url).toBe("www.third.com")
+});
+
+test('i7 escaping bracket should affect detection of url', async () => {
+  const url = await loadFileAndProcessTextForUrl('./input/i7.txt');
+  expect(url).toBe("")
 });
 
 /**
