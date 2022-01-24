86607073a99876b82ffea07814c2375c1e962e7f
Armin Tirić <atab.craft@gmail.com>
2022-01-24 16:48:00 +0100

    feat: find url in text

--- src/url-parse/url-parse.ts
+++ src/url-parse/url-parse.ts
@@ -1,16 +1,29 @@
 import { UrlParseResult } from "./url-parse-result";
 
 export class UrlParse {
-  private URL_EXPRESSION = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+  private URL_EXPRESSION = /https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s{2,}\]|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\]\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s\]]{2,}|www\.[a-zA-Z0-9]+\.[^\s\]]{2,}/gi;
   public URL_REGEX = new RegExp(this.URL_EXPRESSION);
 
   async parseText(text: string): Promise<string> {
-    console.log(text);
-    return "www.google.com"
+    let lParathensis = text.indexOf("[");
+    const rParanthesis = text.lastIndexOf("]");
+
+    //search first bracket you can find that is not escaped
+    while(lParathensis > 0 && text[lParathensis - 1] === "\\") {
+      let positionToStartSearchFor = lParathensis +1;
+      if (positionToStartSearchFor >= text.length - 1) positionToStartSearchFor = text.length;
+      lParathensis = positionToStartSearchFor;
+    }
+    if( lParathensis && rParanthesis){
+      const textBetween = text.substring(lParathensis+1, rParanthesis);
+      const result = [...textBetween.matchAll(this.URL_REGEX)] || [];
+      if ( result?.length == 0) return "";
+      return (result.pop() as RegExpMatchArray)[0];
+    }
+    return ""
   }
 
   async procesUrl(url: string): Promise<UrlParseResult> {
-    console.log(url);
     return new UrlParseResult(url, "title", "email");
   }
 }
